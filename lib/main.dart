a26144d56ef4b5ae1cd29cf94e5a4083b12d6b8d
manan5565 <manankoyawala5565@gmail.com>
2022-08-09 09:58:47 +0530

    Day 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'pages/login_page.dart';
 import 'pages/home_page.dart';
 
 void main() => runApp(
       MaterialApp(
         title: "Flutter Course",
-        home: const HomePage(),
+        // home: const HomePage(),
         debugShowCheckedModeBanner: false,
         themeMode: ThemeMode.light, //for theme mode light, dark or system
         darkTheme: ThemeData(
@@ -12,7 +13,13 @@ void main() => runApp(
           primarySwatch: Colors.red,
         ), //to Apply dark theme properties,
         theme: ThemeData(
-          primarySwatch: Colors.purple,
+          primarySwatch: Colors.deepPurple,
         ), //to apply theme on light mode
+        initialRoute: "/home",
+        routes: {
+          "/": (context) => const LoginPage(),
+          "/home": (context) => const HomePage(),
+          "/login": (context) => const LoginPage(),
+        },
       ),
     );
