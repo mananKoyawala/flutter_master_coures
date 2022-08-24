e3a440293d2a1e99c6244a69cf9b43cdb3116106
manan5565 <manankoyawala5565@gmail.com>
2022-08-24 11:06:20 +0530

    raw cart page created

--- lib/pages/cart.dart
+++ lib/pages/cart.dart
@@ -17,6 +17,79 @@ class CartPage extends StatelessWidget {
           style: TextStyle(color: Theme.of(context).primaryColorLight),
         ),
       ),
+      body: Column(children: const [
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(32.0),
+            child: CartList(),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 32.0),
+          child: Divider(thickness: 2),
+        ),
+        _CartTotal(),
+      ]),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height / 7.5,
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+        Text("\$9999",
+            style: TextStyle(
+                color: Theme.of(context).accentColor,
+                fontSize: 25,
+                fontWeight: FontWeight.w600)),
+        const SizedBox(
+          width: 30,
+        ),
+        SizedBox(
+          height: 50,
+          width: 100,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: const Text(
+              "Buy",
+              style: TextStyle(color: Colors.white),
+            ),
+            style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(Theme.of(context).buttonColor),
+                shape: MaterialStateProperty.all(const StadiumBorder())),
+          ),
+        )
+      ]),
+    );
+  }
+}
+
+class CartList extends StatefulWidget {
+  const CartList({Key? key}) : super(key: key);
+
+  @override
+  State<CartList> createState() => _CartListState();
+}
+
+class _CartListState extends State<CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: const Icon(Icons.done),
+          title: const Text("Items"),
+          trailing: IconButton(
+              onPressed: () {}, icon: const Icon(Icons.remove_circle_outline)),
+        );
+      },
     );
   }
 }
