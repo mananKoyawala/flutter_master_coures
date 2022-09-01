734dd115336496d53bd09a8d64bf6b5effd7d9fa
manan5565 <manankoyawala5565@gmail.com>
2022-09-01 11:55:16 +0530

    User Choice and Splash screen added

---
+++ lib/pages/user_decision_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_master_course/pages/home_page.dart';
+import 'package:flutter_master_course/pages/login_page.dart';
+import 'package:lottie/lottie.dart';
+
+class UserDecisionPage extends StatelessWidget {
+  const UserDecisionPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          Text(
+            "Welcome",
+            style: TextStyle(
+                color: Colors.black, fontSize: 40, fontFamily: 'OpenSans'),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          InkWell(
+            onTap: () => Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (context) => HomePage())),
+            child: Container(
+              height: MediaQuery.of(context).size.height / 8.5,
+              width: MediaQuery.of(context).size.width / 2,
+              child: Center(
+                  child: Text(
+                "Customer".toUpperCase(),
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontFamily: 'QuickSand',
+                    fontWeight: FontWeight.bold),
+              )),
+              decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          InkWell(
+            onTap: () => Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (context) => LoginPage())),
+            child: Container(
+              height: MediaQuery.of(context).size.height / 8.5,
+              width: MediaQuery.of(context).size.width / 2,
+              child: Center(
+                  child: Text(
+                "Merchant".toUpperCase(),
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontFamily: 'QuickSand',
+                    fontWeight: FontWeight.bold),
+              )),
+              decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+          ),
+          SizedBox(
+              height: MediaQuery.of(context).size.height / 5,
+              child: Lottie.asset("assets/jsons/welcomenew.json")),
+        ],
+      ),
+    );
+  }
+}
